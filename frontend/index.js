a12899d23ca8786d165a71c82a22d0eaf28fdd25
ddurov <danyadurovnn@gmail.com>
2022-02-23 23:02:19 +0300

    говно

--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,11 @@
 setInterval(function() {
     let x = new XMLHttpRequest();
-    x.open("POST", "http://localhost:7000/api/mem/abc", true);
+    x.open("POST", "http://localhost:7000/api/memory/info", true);
     x.onload = function() {
         let json = JSON.parse(x.responseText);
         console.log(json);
+        var i = parseInt(Math.floor(Math.log(json.data.available) / Math.log(1024)));
+        console.log(Math.round(json.data.available / Math.pow(1024, i), 2));
     }
     x.onerror = function () {
         console.log("error fetch data");
