bea69aea06de349d14947773f7bdb6bd82e0b9a6
labile <1a6i1e@gmail.com>
2022-05-04 18:39:02 +0300

    fix doc generate unknown type any

--- internal/structures/response.go
+++ internal/structures/response.go
@@ -2,6 +2,6 @@ package structures
 
 // Response is a struct that contains the response from the server.
 type Response struct {
-	Message string `json:"message"`
-	Data    any    `json:"data"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
 }
