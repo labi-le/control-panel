bb05825d60743e9565e7e3824ca1d0b2860cada2
ddurov <danyadurovnn@gmail.com>
2022-05-19 20:01:00 +0300

    added websockets in frontend

--- frontend/index.js
+++ frontend/index.js
@@ -8,10 +8,6 @@ setInterval(function() {
     if (localStorage.getItem("changeTheme") === "auto") (date.getHours() >= 19 && date.getHours() <= 5) ? changeTheme("black") : changeTheme("white");
 }, 60000);
 
-let requests = setInterval(function() {
-    if (localStorage.getItem("nowPage") === "dashboard") updateStatistics();
-}, 1000);
-
 function initLocalStorage() {
     if (localStorage.getItem("firstLoad") === null) {
         localStorage.setItem("firstLoad", "0");
@@ -23,20 +19,18 @@ function initLocalStorage() {
 }
 
 function updateStatistics() {
-    let requestDashboard = new XMLHttpRequest();
-    requestDashboard.open("POST", document.location.protocol + "//" + document.location.hostname + ":" + document.location.port + "/api/dashboard", true);
-    requestDashboard.onload = function () {
-        let dataDashboard = JSON.parse(requestDashboard.responseText);
-        if (dataDashboard.success === false) {
-            showNotify("default", "Возникла ошибка при обработке запроса, попробуйте перезагрузить страницу");
-            clearInterval(requests);
-        }
+    let webSocket = new WebSocket("ws://" + document.location.hostname + ":" + document.location.port + "/ws/dashboard");
 
+    webSocket.addEventListener('open', function (event) {
+        webSocket.send(JSON.stringify({"path": "/"}));
+    });
+    webSocket.addEventListener('message', function (event) {
+        let dataDashboard = JSON.parse(event.data)[0];
         // memory info
-        document.getElementById("totalMemVal").innerHTML = formatBytes(dataDashboard.mem.total);
-        document.getElementById("usedMemVal").innerHTML = formatBytes(dataDashboard.mem.used);
-        document.getElementById("freeMemVal").innerHTML = formatBytes(dataDashboard.mem.free);
-        document.getElementById("cachedMemVal").innerHTML = formatBytes(dataDashboard.mem.cached);
+        document.getElementById("totalMemVal").innerHTML = "Всего: "+formatBytes(dataDashboard.mem.total);
+        document.getElementById("usedMemVal").innerHTML = "Используется: "+formatBytes(dataDashboard.mem.used);
+        document.getElementById("freeMemVal").innerHTML = "Свободно: "+formatBytes(dataDashboard.mem.free);
+        document.getElementById("cachedMemVal").innerHTML = "Кэш: "+formatBytes(dataDashboard.mem.cached);
         // CPU info
         document.getElementById("cpuLoadValue").innerHTML = parseFloat(dataDashboard.cpu_load.load).toFixed(2) + "%";
         // disk info
@@ -46,11 +40,10 @@ function updateStatistics() {
         document.getElementById("usedDiskSizeValue").innerHTML = "-- Используется: " + formatBytes(dataDashboard.io.used);
         document.getElementById("usedDiskSizeInPercentValue").innerHTML = "-- Используется (%): " + parseFloat(dataDashboard.io.usedPercent).toFixed(2) + "%";
         // update api/panel version
-        document.getElementById("footer-version").innerHTML = "v" + requestDashboard.getResponseHeader("Version");
-    };
-    requestDashboard.onerror = function () {
-        showNotify("default", "Возникла ошибка при обработке запроса, попробуйте перезагрузить страницу");
-        clearInterval(requests);
-    }
-    requestDashboard.send(JSON.stringify({"path": "/"}));
+        document.getElementById("footer-version").innerHTML = "v1";
+    });
+    webSocket.addEventListener('close', function (event) {
+        showWindow("default", "Возникла ошибка при обработке запроса, попробуйте перезагрузить страницу");
+        webSocket.close();
+    });
 }
