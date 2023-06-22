33e2bfffdf4e39989a5652449da1e58004b8ae5e
labile <1a6i1e@gmail.com>
2023-06-22 20:06:02 +0300

    не используем дб для хранения настроек

--- internal/settings.go
+++ internal/settings.go
@@ -2,57 +2,44 @@ package internal
 
 import (
 	"fmt"
-	"github.com/sirupsen/logrus"
-	"gorm.io/driver/sqlite"
-	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
+	"github.com/BurntSushi/toml"
+	"github.com/rs/zerolog/log"
+	"io"
 	"os"
 	"time"
-)
-
-var (
-	DefaultConfigPath   = os.Getenv("HOME") + "/.config/control-panel/"
-	DefaultSettingsFile = "settings"
 )
 
 const (
 	PanelVersion = "0.0.1"
 
-	DefaultLanguage               = "en"
-	DefaultLogLevel               = logrus.DebugLevel
-	DefaultAddr                   = "0.0.0.0"
-	DefaultPort                   = "7777"
-	DefaultDashboardUpdateTimeout = time.Second * 1
-
 	ProductionStaticPath = "/opt/control-panel/static/"
 	DevelopStaticPath    = "./frontend/"
+	DefaultConfigName    = "config.toml"
 )
 
 // PanelSettings
 // port - the port on which the panel will work
 // logLevel - the debug level of the panel
 // language - panel language
-// DashboardUpdateTimeout - the timeout of updates dashboard
+// DashboardDelay - the timeout of updates dashboard
 type PanelSettings struct {
-	Addr                   string        `json:"addr"`
-	Port                   string        `json:"port"`
-	LogLevel               logrus.Level  `json:"log_level"`
-	Language               string        `json:"language"`
-	DashboardUpdateTimeout time.Duration `json:"dashboard_update_timeout"`
-
-	dbConn *gorm.DB
+	Addr           string        `toml:"addr"`
+	Port           int           `toml:"port"`
+	LogLevel       string        `toml:"log_level"`
+	Language       string        `toml:"language"`
+	DashboardDelay time.Duration `toml:"dashboard_delay"`
 }
 
 func (p *PanelSettings) GetAddr() string {
 	return p.Addr
 }
 
-func (p *PanelSettings) GetPort() string {
+func (p *PanelSettings) GetPort() int {
 	return p.Port
 }
 
 func (p *PanelSettings) GetLogLevel() string {
-	return p.LogLevel.String()
+	return p.LogLevel
 }
 
 func (p *PanelSettings) GetLanguage() string {
@@ -60,78 +47,60 @@ func (p *PanelSettings) GetLanguage() string {
 }
 
 func (p *PanelSettings) GetDashboardUpdateTimeout() time.Duration {
-	return p.DashboardUpdateTimeout
+	return p.DashboardDelay
 }
 
 func NewPanelSettings(settingsPath string) (*PanelSettings, error) {
 	if settingsPath == "" {
-		settingsPath = DefaultConfigPath
+		settingsPath = DefaultConfigPath()
 	}
 
-	if _, err := os.Stat(settingsPath + DefaultSettingsFile); os.IsNotExist(err) {
-		err := os.MkdirAll(settingsPath, os.ModePerm)
+	var (
+		file     io.Reader
+		settings PanelSettings
+	)
+
+	log.Debug().Msgf("Settings path: %s", settingsPath)
+	if _, err := os.Stat(settingsPath + DefaultConfigName); os.IsNotExist(err) {
+		file, err = createConfigFile(settingsPath, DefaultConfigName, DefaultPanelSettings())
 		if err != nil {
 			return nil, err
 		}
 	}
 
-	conn, err := gorm.Open(
-		sqlite.Open(settingsPath+DefaultSettingsFile),
-		&gorm.Config{Logger: logger.Default.LogMode(logger.Silent)},
-	)
+	file, err := os.Open(settingsPath + DefaultConfigName)
 	if err != nil {
 		return nil, err
 	}
-	if err = conn.AutoMigrate(&PanelSettings{}); err != nil {
+
+	if _, err := toml.NewDecoder(file).Decode(&settings); err != nil {
+		return nil, err
+	}
+	return &settings, err
+}
+
+func createConfigFile(path, name string, settings *PanelSettings) (io.Reader, error) {
+	if err := os.MkdirAll(path, os.ModePerm); err != nil {
 		return nil, err
 	}
 
-	return (&PanelSettings{dbConn: conn}).GetSettings()
+	file, err := os.Create(path + name)
+	if err != nil {
+		return nil, err
+	}
+
+	return file, toml.NewEncoder(file).Encode(settings)
+
 }
 
-// DefaultPanelSettings
-// ErrorLevels:
-// panic
-// fatal
-// error
-// warning
-// info
-// debug
-// trace
 func DefaultPanelSettings() *PanelSettings {
 	return &PanelSettings{
-		Addr:                   DefaultAddr,
-		Port:                   DefaultPort,
-		LogLevel:               DefaultLogLevel,
-		Language:               DefaultLanguage,
-		DashboardUpdateTimeout: DefaultDashboardUpdateTimeout,
+		Addr:           "0.0.0.0",
+		Port:           7777,
+		LogLevel:       "info",
+		Language:       "en",
+		DashboardDelay: time.Second,
 	}
-}
-
-// GetSettings returns the settings
-// if the settings are not found, it will save and return default settings
-func (p *PanelSettings) GetSettings() (*PanelSettings, error) {
-	if err := p.dbConn.Where("_rowid_ = ?", 1).First(p).Error; err != nil {
-		if err = p.dbConn.Create(DefaultPanelSettings()).Error; err != nil {
-			return nil, err
-		} else {
-			pp := DefaultPanelSettings()
-			pp.dbConn = p.dbConn
-
-			return pp, nil
-		}
-	}
-
-	return p, nil
-}
-
-// UpdateSettings updates the settings
-func (p *PanelSettings) UpdateSettings(settings PanelSettings) error {
-	return p.dbConn.Model(&settings).Where("_rowid_ = ?", 1).Updates(&settings).Error
-}
-
-func (p *PanelSettings) ResetSettings() error {
-	return p.UpdateSettings(*DefaultPanelSettings())
 }
 
 // String returns config as string
@@ -143,4 +112,8 @@ func (p *PanelSettings) String() string {
 		p.Addr,
 		p.Port,
 	)
+}
+
+func DefaultConfigPath() string {
+	return os.Getenv("HOME") + "/.config/control-panel/"
 }
